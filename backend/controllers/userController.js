5f1e1c3c7cbaf5c573a309d4bd020170567e361f
Tausif104 <tausifahmed49@gmail.com>
2022-04-05 03:26:30 +0600

    login registration goals done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,21 +23,23 @@ const registerUser = asyncHandler(async (req, res) => {
 	}
 
 	// hash the password
-	const salt = await bcrypt.getSalt(10)
+	const salt = await bcrypt.genSalt(10)
 	const hashedPassword = await bcrypt.hash(password, salt)
 
 	// create user
 	const user = await User.create({
 		name,
 		email,
-		hashedPassword,
+		password: hashedPassword,
 	})
 
 	if (user) {
+		console.log(user)
 		res.status(201).json({
 			_id: user.id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user._id),
 		})
 	} else {
 		res.status(400)
@@ -49,15 +51,41 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @assess  public
 const loginUser = asyncHandler(async (req, res) => {
-	res.json({ message: 'Login user' })
+	const { email, password } = req.body
+	const user = await User.findOne({ email })
+
+	if (user && (await bcrypt.compare(password, user.password))) {
+		res.json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user._id),
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid Credentials')
+	}
 })
 
 // @desc    Get user data
 // @route   DELETE /api/users/me
-// @assess  public
+// @assess  private
 const getMe = asyncHandler(async (req, res) => {
-	res.json({ message: 'User data display' })
+	const { _id, name, email } = await User.findById(req.user.id)
+
+	res.status(200).json({
+		id: _id,
+		name,
+		email,
+	})
 })
+
+// generate jwt
+const generateToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: '30d',
+	})
+}
 
 module.exports = {
 	registerUser,
